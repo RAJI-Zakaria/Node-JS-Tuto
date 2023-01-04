1be86f28dce3c5959c3bd8ac37b09c1a62a1c6b8
RAJI-Zakaria <zakariaraji.me@gmail.com>
2023-01-04 11:42:33 +0100

    Hello world

---
+++ 1. Intro/app.js
@@ -0,0 +1,8 @@
+function sayHello(name){
+    console.log('Hello '+name);
+}
+
+sayHello('Zakaria');
+
+//we cant use window because we are not executing the programme on browser so we don't have window nor document.....
+//console.log(window);
