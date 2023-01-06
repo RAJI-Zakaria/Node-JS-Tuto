dd335710a7370765aa9d49659b93c1f1ee60b7ab
RAJI-Zakaria <zakariaraji.me@gmail.com>
2023-01-06 15:00:51 +0100

    Handling HTTP Delete Requests

--- 4. Building RESTful API's Using Express/index.js
+++ 4. Building RESTful API's Using Express/index.js
@@ -84,6 +84,22 @@ app.put('/api/courses/:id', (req,res)=>{
     res.send(course);
 })
 
+app.delete('/api/courses/:id', (req,res)=>{
+
+    const course = courses.find((c) => c.id===parseInt(req.params.id))
+
+    if(!course) return res.status(404).send('the course with the giving id was not found')
+
+
+    //Delete
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+
+
+});
+
 function validateCourse(course){
     const schema = Joi.object({
         name:Joi.string().min(3).required()
