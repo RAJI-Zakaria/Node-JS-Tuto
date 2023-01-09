76655a13d1e9fbec97c9043c3dbb6736267568e6
RAJI-Zakaria <zakariaraji.me@gmail.com>
2023-01-09 15:11:43 +0100

    Middleware - Customised...
    why use Middleware?

---
+++ 5. Express- Advanced Topics/app.js
@@ -0,0 +1,121 @@
+//A middleware function is a function that takes a request object and either terminates the request/response cycle or passes control to another middleware function.
+//its used to handle
+
+
+const express = require('express');
+const app = express();
+
+const Joi = require('joi');
+
+const logger = require('./logger.js');
+const PORT = process.env.port || 3000
+
+
+//express.json() : returns a middleware function.....
+app.use(express.json());
+
+//we are talking about request processsing pipeline : contains differents middleware such json() & route()....
+
+//this concept is meant to be used for loggin, authorization....
+
+//Creating a middleware fucntion :
+app.use(logger)
+
+const courses = [
+    {id:1,name:"course1"},
+    {id:2,name:"course2"},
+    {id:3,name:"course3"}
+];
+
+
+
+app.get('/api/courses', (req,res)=>{
+    res.send(courses);
+})
+
+
+
+app.get('/api/courses/:id', (req,res)=>{
+    const course = courses.find((c) => c.id===parseInt(req.params.id))
+    if(!course)//return 404
+        res.status(404).send('the course with the giving id was not found')
+
+    res.send(course)
+})
+
+
+app.post('/api/courses', (req,res)=>{
+    //validating
+    const {error} = validateCourse(req.body);
+    //checking the value of our property "name" :
+    if(error){//not null == error occurs
+        //it's better to send a specific error data instead of sending the whole object Ex : rs.error ==> rs.error.details[0].message
+        //note : we can concatenate selected error data.
+        res.status(400).send(error.details[0].message);
+        //die or exit
+        return;
+    }
+    //in order for the "body" to work we must use expressJson middleware
+    const course = {
+        id:courses.length+1,
+        name:req.body.name
+    }
+    courses.push(course);
+    //return the new course with id to the client
+    res.send(course);
+})
+
+app.put('/api/courses/:id', (req,res)=>{
+    //Look up the course by id
+    //if not existing, return 404
+    const course = courses.find((c) => c.id===parseInt(req.params.id))
+    if(!course)//return 404
+    {
+        res.status(404).send('the course with the giving id was not found')
+        return;
+    }
+
+    //validate
+    //If invalid, return 400 - Bad request
+    //using object destructuring { ... }
+    const {error} = validateCourse(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    //update course
+    //return the updated course
+    course.name=req.body.name;
+    res.send(course);
+})
+
+app.delete('/api/courses/:id', (req,res)=>{
+
+    const course = courses.find((c) => c.id===parseInt(req.params.id))
+
+    if(!course) return res.status(404).send('the course with the giving id was not found')
+
+
+    //Delete
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+
+
+});
+
+function validateCourse(course){
+    const schema = Joi.object({
+        name:Joi.string().min(3).required()
+    })
+
+    return schema.validate(course);
+}
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}....`);
+});
---
+++ 5. Express- Advanced Topics/logger.js
@@ -0,0 +1,9 @@
+
+
+function log(req,res,next){
+    console.log('Logging...');
+    //if "next()' not used, the app will hang in there.. will not respond to the request
+    next();
+}
+
+module.exports = log;
